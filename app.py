a5e97e0f4ca3789899b24b8daccbab6bb292cf61
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2022-08-27 15:02:29 -0300

    App VFinal

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - To no Docker"
+    return "Hello World - ACABOUUU :("
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
